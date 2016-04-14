5c1f9ead44e77e46fabcdc6f2806eb0c2aa1168f
JJ Merelo <jjmerelo@gmail.com>
2016-04-14 18:38:04 +0200

    Adaptados los scripts a la nueva versión de ggplot2
    Y relanzados los gráficos. qplot simplemente no admite barras
    closes #1

--- R/qplot.R
+++ R/qplot.R
@@ -3,5 +3,6 @@ library(ggplot2)
 elecciones <- read.csv('../data/resultadoselecciones2015.csv')
 qplot(elecciones$Sector, elecciones$Votos,color=elecciones$Candidato)
 qplot(elecciones$Sector, elecciones$Votos,color=elecciones$Candidato,size=elecciones$Votos)
-qplot(data=elecciones, x=Sector, y=Votos,geom="bar",stat="identity", fill=Candidato)
-qplot(data=elecciones, x=Sector, y=Votos,geom="bar",stat="identity", fill=Candidato, facets=Candidato~Sector)
+
+ggplot(data=elecciones, aes(x=Sector, y=Votos, fill=Candidato))+geom_bar(stat="identity")
+qplot(data=elecciones, x=Sector, y=Votos, shape=Candidato, facets=Candidato~Sector)
