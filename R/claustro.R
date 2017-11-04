4b1f5c79bbe8485f11521bd778293eae350fcad8
JJ Merelo <jjmerelo@gmail.com>
2017-11-04 16:48:13 +0100

    He cambiado algo raruno

--- R/claustro.R
+++ R/claustro.R
@@ -4,6 +4,8 @@ library(plotly)
 
 votos.claustro <- read.csv("../data/claustro.csv")
 
+votos.plot <- ggplot()+geom_bar(data=votos.claustro,aes(x=reorder(Nombre,-Votos),y=Votos,fill=Votos),stat='identity')+theme_tufte()+theme(axis.text.x = element_text(angle = 90, hjust = 1))+labs("Votos a claustro ETSIIT",x="Candidatos")
+
 votos.plot <- ggplot() +geom_bar(data=votos.claustro,aes(x=reorder(Nombre,-Votos),y=Votos,fill=Votos),stat='identity')
 
 votos.plot+theme(axis.text.x = element_text(angle = 90, hjust = 1))+labs("Votos a claustro ETSIIT",x="Candidatos")
